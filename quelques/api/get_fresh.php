6fbf467aa61abcc10a57dc44d6d1cb861fa66411
jacquarg <guillaume.jacquart@hoodbrains.com>
2021-06-23 08:57:12 +0200

    Demo Quelques

---
+++ quelques/api/get_fresh.php
@@ -0,0 +1,13 @@
+<?php
+    require 'fetch_one.php';
+
+    $json = fetchOne();
+    $path = "../data/live/".date('Y\/m\/d\.\t\x\t');
+    mkdir(dirname($path), 0755, true);
+    file_put_contents($path, $json.PHP_EOL , FILE_APPEND | LOCK_EX);
+
+
+    header("Content-Type: application/json; charset=UTF-8");
+    http_response_code(200);
+    echo $json
+?>
